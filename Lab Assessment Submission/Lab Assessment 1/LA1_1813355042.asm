fa46efba5fce51d7ed470f3378e80407c036f019
Durdana Kamal <74124068+durdana-kamal@users.noreply.github.com>
2020-11-22 11:22:05 +0600

    Lab assessment submission

---
+++ Lab Assessment Submission/Lab Assessment 1/LA1_1813355042.asm
@@ -0,0 +1,23 @@
+
+
+
+org 100h   
+
+; 02 --- 02H;
+; int main () {
+
+MOV AX, 02
+MOV BX, 02
+ADD AX, BX
+
+; AX = AX+BX     int a=7, b=1, c=0;
+; c = a+b;
+; printf (c);
+; return 0;
+;
+
+ret
+
+
+
+
